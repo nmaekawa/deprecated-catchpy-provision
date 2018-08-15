136781ab692d62584333e1f27f55727429a793ae
nmaekawa <nmaekawa@g.harvard.edu>
2018-08-15 11:36:08 -0400

    use cloudwatch ansible-role
    - keep local cloudwatch for catchpy-custom metrics

--- roles/cloudwatch/templates/catchpy_response_time_metric.sh
+++ roles/cloudwatch/templates/catchpy_response_time_metric.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-. {{ org_rootdir }}/bin/custom_metrics_shared.sh
+. {{ cloudwatch_user_home }}/bin/custom_metrics_shared.sh
 
 metric_name="WebserviceResponseTime"
 
-catch_jwt=$({{ service_venv_dir }}/bin/python {{ org_rootdir }}/bin/make_jwt.py {{ cloudwatch_api_key }} {{ cloudwatch_secret_key }} {{ cloudwatch_user }})
+catch_jwt=$({{ service_venv_dir }}/bin/python {{ cloudwatch_user_home }}/bin/make_jwt.py {{ cloudwatch_api_key }} {{ cloudwatch_secret_key }} {{ cloudwatch_user }})
 
 response_time=$(curl -w "%{time_total}" \
     --silent \
     -H "Authorization: token ${catch_jwt}" \
     {{ (enable_ssl == "true") | ternary("https", "http") }}://{{ webserver_dns }}/is_alive \
-    -o {{ org_rootdir }}/tmp/cloudwatch/is_alive_response.json)
+    -o {{ cloudwatch_user_home }}/tmp/cloudwatch/is_alive_response.json)
 
 # check that payload is ok from is_alive response; needs to install jq
 # or assume that responding means ok
-is_alive=$(cat {{ org_rootdir }}/tmp/cloudwatch/is_alive_response.json | jq '.payload[0] == "ok"')
+is_alive=$(cat {{ cloudwatch_user_home }}/tmp/cloudwatch/is_alive_response.json | jq '.payload[0] == "ok"')
 # then put-metric-data ${response_time} to cloudwatch
 if [ ${is_alive} == 'true' ]; then
   /usr/bin/aws cloudwatch put-metric-data \
@@ -27,5 +27,5 @@ if [ ${is_alive} == 'true' ]; then
 fi
 
 # remove output file
-rm /opt/hx/tmp/cloudwatch/is_alive_response.json
+rm {{ cloudwatch_user_home }}/tmp/cloudwatch/is_alive_response.json
 
--- roles/cloudwatch/templates/custom_metrics_shared.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-namespace="HX/CustomMetrics/{{ service_name }}"
-region="us-west-2"
-instance_id="{{ ansible_ec2_instance_id }}"
